f9cc0e939a821834bb78060d14344f48395b4b49
AntthonyQQ <aquiangquiros2025fwd@gmail.com>
2025-11-11 15:21:23 -0600

    Ajuste de urls

--- Rubys_Foundation/api/urls.py
+++ Rubys_Foundation/api/urls.py
@@ -6,9 +6,9 @@ from .views import (
     UsersTypeRetrieveUpdateDestroyView)
 
 urlpatterns = [
-    path('Users/', UsersListCreateView.as_view(), name="Get and Post Users"),
-    path('Users/<int:pk>', UsersRetrieveUpdateDestroyView.as_view(), name="Put delete byid  Users"),
+    path('users/', UsersListCreateView.as_view(), name="Get and Post Users"),
+    path('users/<int:pk>', UsersRetrieveUpdateDestroyView.as_view(), name="Put delete byid  Users"),
 
-    path('UsersType/', UsersTypeListCreateView.as_view(), name="Get and Post UsersType"),
-    path('UsersType/<int:pk>', UsersTypeRetrieveUpdateDestroyView.as_view(), name="Put delete byid  UsersType")
+    path('userstype/', UsersTypeListCreateView.as_view(), name="Get and Post UsersType"),
+    path('userstype/<int:pk>', UsersTypeRetrieveUpdateDestroyView.as_view(), name="Put delete byid  UsersType")
 ]
